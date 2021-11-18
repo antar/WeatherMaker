332d8ec4173bf1fd94eb0229f0a649cd84f2cfec
Alessio Carcavallo <alecar@az.zli.ch>
2021-11-18 16:24:43 +0100

    fix: learn how to format code

--- App/app/src/main/java/weathermaker/WeatherData.java
+++ App/app/src/main/java/weathermaker/WeatherData.java
@@ -29,9 +29,9 @@ public class WeatherData {
 
     private static String updateWeatherIcon(int condition) {
         if (condition >= 0 && condition <= 300) { return "thunderstrom1"; }
-        else if (condition >= 300 && condition<=500) { return "lightrain"; }
-        else if (condition >= 500 && condition<=600) { return "shower"; }
-        else if (condition >= 600 && condition<=700) { return "snow2"; }
+        else if (condition >= 300 && condition <= 500) { return "lightrain"; }
+        else if (condition >= 500 && condition <= 600) { return "shower"; }
+        else if (condition >= 600 && condition <= 700) { return "snow2"; }
         else if (condition >= 701 && condition <= 771) { return "fog"; }
         else if (condition >= 772 && condition <= 800) { return "overcast"; }
         else if(condition == 800) { return "sunny"; }
@@ -44,7 +44,7 @@ public class WeatherData {
     }
 
     public String getTemperature() {
-        return temperature+"°C";
+        return temperature + "°C";
     }
 
     public String getIcon() {
