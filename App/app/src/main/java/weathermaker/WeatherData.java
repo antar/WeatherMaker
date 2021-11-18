14e3f331c2241790e12bcc92ce076a1ba31a61e1
Alessio Carcavallo <alecar@az.zli.ch>
2021-11-18 16:22:55 +0100

    feat: added method for updating the Weather-Icon

--- App/app/src/main/java/weathermaker/WeatherData.java
+++ App/app/src/main/java/weathermaker/WeatherData.java
@@ -10,15 +10,15 @@ public class WeatherData {
 
     public static WeatherData fromJson(JSONObject jsonObject) {
         try {
-            WeatherData weatherD = new WeatherData();
-            weatherD.city = jsonObject.getString("name");
-            weatherD.condition = jsonObject.getJSONArray("weather").getJSONObject(0).getInt("id");
-            weatherD.weatherType = jsonObject.getJSONArray("weather").getJSONObject(0).getString("main");
-            //weatherD.icon = IconFunction(weatherD.condition);
+            WeatherData weather = new WeatherData();
+            weather.city = jsonObject.getString("name");
+            weather.condition = jsonObject.getJSONArray("weather").getJSONObject(0).getInt("id");
+            weather.weatherType = jsonObject.getJSONArray("weather").getJSONObject(0).getString("main");
+            weather.icon = updateWeatherIcon(weather.condition);
             double tempResult = jsonObject.getJSONObject("main").getDouble("temp")-273.15;
             int roundedValue = (int)Math.rint(tempResult);
-            weatherD.temperature = Integer.toString(roundedValue);
-            return weatherD;
+            weather.temperature = Integer.toString(roundedValue);
+            return weather;
         }
         catch (JSONException e) {
             e.printStackTrace();
@@ -26,4 +26,36 @@ public class WeatherData {
         }
     }
 
+
+    private static String updateWeatherIcon(int condition) {
+        if (condition >= 0 && condition <= 300) { return "thunderstrom1"; }
+        else if (condition >= 300 && condition<=500) { return "lightrain"; }
+        else if (condition >= 500 && condition<=600) { return "shower"; }
+        else if (condition >= 600 && condition<=700) { return "snow2"; }
+        else if (condition >= 701 && condition <= 771) { return "fog"; }
+        else if (condition >= 772 && condition <= 800) { return "overcast"; }
+        else if(condition == 800) { return "sunny"; }
+        else if(condition >= 801 && condition <= 804) { return "cloudy"; }
+        else if(condition >= 900 && condition <= 902) { return "thunderstrom1"; }
+        if(condition == 903) { return "snow1"; }
+        if (condition == 904) { return "sunny"; }
+        if (condition >= 905 && condition <= 1000) { return "thunderstrom2"; }
+        return "dunno";
+    }
+
+    public String getTemperature() {
+        return temperature+"°C";
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getWeatherType() {
+        return weatherType;
+    }
 }
